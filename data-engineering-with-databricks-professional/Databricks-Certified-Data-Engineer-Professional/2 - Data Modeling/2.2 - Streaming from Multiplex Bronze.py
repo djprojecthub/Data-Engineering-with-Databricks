07e9ca822a3778d24ea0136e99da8c6266401c4e
djprojecthub <djprojecthub>
2025-01-13 12:51:17 +0000

    basic of silver

--- data-engineering-with-databricks-professional/Databricks-Certified-Data-Engineer-Professional/2 - Data Modeling/2.2 - Streaming from Multiplex Bronze.py
+++ data-engineering-with-databricks-professional/Databricks-Certified-Data-Engineer-Professional/2 - Data Modeling/2.2 - Streaming from Multiplex Bronze.py
@@ -1,9 +1,8 @@
 # Databricks notebook source
 # MAGIC %md-sandbox
 # MAGIC
-# MAGIC <div  style="text-align: center; line-height: 0; padding-top: 9px;">
-# MAGIC   <img src="https://raw.githubusercontent.com/derar-alhussein/Databricks-Certified-Data-Engineer-Professional/main/Includes/images/orders.png" width="60%">
-# MAGIC </div>
+# MAGIC # Overview
+# MAGIC In this notebook, we are going to parse raw data from single topic in our multiplex bronze table. We will create orders **silver** table.
 
 # COMMAND ----------
 
@@ -11,10 +10,23 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC Cast **key** and **value** columns to see the actual data. **Value** column has data in JSON format.
+
+# COMMAND ----------
+
 # MAGIC %sql
 # MAGIC SELECT cast(key AS STRING), cast(value AS STRING)
-# MAGIC FROM bronze
+# MAGIC FROM bronze WHERE topic='orders'
 # MAGIC LIMIT 20
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Static read
+# MAGIC
+# MAGIC - Reads **orders** topic data from bookstore dataset stored in bronze table. 
+# MAGIC - The **from_json()** function parses the data in the given schema. 
 
 # COMMAND ----------
 
@@ -27,9 +39,20 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## Streaming read
+# MAGIC Convert the static bronze table to streaming temporary view. This allow use to write streaming queries with Spark SQL.
+
+# COMMAND ----------
+
 (spark.readStream
       .table("bronze")
       .createOrReplaceTempView("bronze_tmp"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Change the reference of above static read query to the streaming temporary view **bronze_tmp**.
 
 # COMMAND ----------
 
@@ -39,6 +62,17 @@
 # MAGIC   SELECT from_json(cast(value AS STRING), "order_id STRING, order_timestamp Timestamp, customer_id STRING, quantity BIGINT, total BIGINT, books ARRAY<STRUCT<book_id STRING, quantity BIGINT, subtotal BIGINT>>") v
 # MAGIC   FROM bronze_tmp
 # MAGIC   WHERE topic = "orders")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Remember such always on stream prevents cluster from auto-termination.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Capture the SQL logic in a temporary view
+# MAGIC We can use this temporary view to switch from SQL to Python anytime.
 
 # COMMAND ----------
 
@@ -52,6 +86,13 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## Create silver table
+# MAGIC - Using streaming write function **writeStream()** to persist the result of temporary view to disk or any other cloud storage location.
+# MAGIC - **trigger(availableNow=True)** specifies that all data will be processed in multiple microbatches until no more data is available and then stop the stream.
+
+# COMMAND ----------
+
 query = (spark.table("orders_silver_tmp")
                .writeStream
                .option("checkpointLocation", "dbfs:/mnt/demo_pro/checkpoints/orders_silver")
@@ -59,6 +100,11 @@ query = (spark.table("orders_silver_tmp")
                .table("orders_silver"))
 
 query.awaitTermination()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Complete logic using Python API
 
 # COMMAND ----------
 
--- data-engineering-with-databricks-professional/Databricks-Certified-Data-Engineer-Professional/2 - Data Modeling/2.3 - Quality Enforcement.py
+++ data-engineering-with-databricks-professional/Databricks-Certified-Data-Engineer-Professional/2 - Data Modeling/2.3 - Quality Enforcement.py
@@ -1,5 +1,16 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ### Overview
+# MAGIC In this notebook we are going to see how to add check constraint to delta table to improve quality of our data.
+
+# COMMAND ----------
+
 # MAGIC %run ../Includes/Copy-Datasets
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### We can define constraint on our existing delta table
 
 # COMMAND ----------
 
@@ -8,8 +19,20 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC #### Table constraints are listed under the DESCRIBE EXTENDED
+
+# COMMAND ----------
+
 # MAGIC %sql
 # MAGIC DESCRIBE EXTENDED orders_silver
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### TEST VIOLATION
+# MAGIC
+# MAGIC The second record violates the constraint. Let see what happens.
 
 # COMMAND ----------
 
@@ -21,6 +44,12 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ### Atomic transaction
+# MAGIC All transaction in the delta lake table are atomic which means none of the rows will be inserted if the transaction fails.
+
+# COMMAND ----------
+
 # MAGIC %sql
 # MAGIC SELECT *
 # MAGIC FROM orders_silver
@@ -28,8 +57,20 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ### Add another constraint
+# MAGIC Check all order has quantity greater than 0. Note that the error also tells how many rows violates the contraint.
+
+# COMMAND ----------
+
 # MAGIC %sql
 # MAGIC ALTER TABLE orders_silver ADD CONSTRAINT valid_quantity CHECK (quantity > 0);
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Is the new constraint listed?
+# MAGIC No, because no data violating the contraint should already be in the table before defining the new constraint.
 
 # COMMAND ----------
 
@@ -42,6 +83,12 @@
 # MAGIC SELECT *
 # MAGIC FROM orders_silver
 # MAGIC where quantity <= 0
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Python API
+# MAGIC As a best practice we should always remove the bad records and then add data to silver table. This way we will be to add check constraints without any error.
 
 # COMMAND ----------
 
@@ -63,6 +110,12 @@ query.awaitTermination()
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ### Drop constraint
+# MAGIC
+
+# COMMAND ----------
+
 # MAGIC %sql
 # MAGIC ALTER TABLE orders_silver DROP CONSTRAINT timestamp_within_range;
 
@@ -73,13 +126,14 @@ query.awaitTermination()
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC We will add more transformation in the next lectures so dropping the orders_silver for now and removing its checkpoint.
+
+# COMMAND ----------
+
 # MAGIC %sql
 # MAGIC DROP TABLE orders_silver
 
 # COMMAND ----------
 
 dbutils.fs.rm("dbfs:/mnt/demo_pro/checkpoints/orders_silver", True)
-
-# COMMAND ----------
-
-
